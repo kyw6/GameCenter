5b0b8713360c16ed6128c3b7508ffd69aa408420
makang <makang3@xiaomi.com>
2024-08-12 11:30:34 +0800

    添加首页搜索框样式 谈ä完善闪屏页面å弹窗关闭 完善闪屏页面状态栏

--- app/src/main/java/com/example/gamecenter/SplashActivity.java
+++ app/src/main/java/com/example/gamecenter/SplashActivity.java
@@ -26,14 +26,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 设置状态栏颜色
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.setStatusBarColor(Color.TRANSPARENT);
-            window.getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            );
-        }
+        Window window = getWindow();
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR // 图标是深色的
+        );
         setContentView(R.layout.activity_splash);
 
         PreferencesUtil.init(this);
@@ -44,8 +44,8 @@ public class SplashActivity extends AppCompatActivity {
                 startMainActivity();
             } else {
                 // 显示DialogFragment
-                    SplashDialogFragment dialogFragment = new SplashDialogFragment();
-                    dialogFragment.show(getSupportFragmentManager(), "MyDialogFragment");
+                SplashDialogFragment dialogFragment = new SplashDialogFragment();
+                dialogFragment.show(getSupportFragmentManager(), "MyDialogFragment");
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
