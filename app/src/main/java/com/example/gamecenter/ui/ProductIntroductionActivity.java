1ae4396729e89abfbb557652fbfb47a12de27806
makang <makang3@xiaomi.com>
2024-08-15 16:50:42 +0800

    新增产品介绍页面

---
+++ app/src/main/java/com/example/gamecenter/ui/ProductIntroductionActivity.java
@@ -0,0 +1,14 @@
+package com.example.gamecenter.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProductIntroductionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
